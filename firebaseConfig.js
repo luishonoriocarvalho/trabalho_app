20dbd1fda3eef8cbe1957606b17bfe2bb4530af4
Luís Honório Silveira Carvalho <116170242+luishonoriocarvalho@users.noreply.github.com>
2024-10-21 16:18:25 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,7 @@ import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/aut
 import AsyncStorage from '@react-native-async-storage/async-storage'; // Importa o AsyncStorage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAC1CwAZzzbhcaphu7BLUo0fSgIpFAkLVg",
-  authDomain: "app-financa-b717b.firebaseapp.com",
-  databaseURL: "https://app-financa-b717b-default-rtdb.firebaseio.com",
-  projectId: "app-financa-b717b",
-  storageBucket: "app-financa-b717b.appspot.com",
-  messagingSenderId: "502577923686",
-  appId: "1:502577923686:web:f79e7f1c13c0d7d7e40fa7"
+  //pegar no firebas
 };
 
 // Inicializa o Firebase com persistência usando o AsyncStorage
